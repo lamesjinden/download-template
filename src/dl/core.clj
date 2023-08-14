78580078d7c15457261f2801437dc7fbf50299ca
james linden <7298563+lamesjinden@users.noreply.github.com>
2023-08-13 21:33:56 -0400

    Add 'continueOnError', incomplete downloads write to a '.part' file until complete, logging through Timbre;

--- src/dl/core.clj
+++ src/dl/core.clj
@@ -3,9 +3,12 @@
 (ns dl.core
   (:require [clojure.java.io :as io]
             #_[clojure.string :as str]
-            #_[babashka.fs :as fs]
-            [babashka.http-client :as http])
+            [babashka.fs :as fs]
+            [babashka.http-client :as http]
+            [taoensso.timbre :as timbre :refer [info infof debug debugf error errorf trace tracef]])
   #_(:import [java.net URLEncoder]))
+
+(def continueOnError true)
 
 (def sources [{:url "https://path/to/subject"
                :out "./path/to/destination.file"}])
@@ -17,32 +20,45 @@
 (defn download [url options]
   (http/get url options))
 
-(defn download-file [url destination]
+(defn download-file [^String url ^String destination]
   (let [options default-http-options
         response (download url options)
-        source-stream (:body response)]
-    (with-open [destination-stream (->> destination
-                                        io/as-file
-                                        io/output-stream)]
-      (io/copy
-       source-stream
-       destination-stream))))
+        source-stream (:body response)
+        destination-part (str destination ".part")]
+    (try
+      (with-open [destination-stream (->> destination-part
+                                          io/as-file
+                                          io/output-stream)]
+        (io/copy source-stream destination-stream))
+      (fs/move destination-part destination {:replace-existing true})
+      (finally
+        (fs/delete-if-exists destination-part)))))
 
 (defn run []
-  (println "downloading...")
+  (info "downloading...")
   (let [downloadables (->> sources
+                           ;; note - transform sources here
                            (filter #(when % %))
-                           (map identity))]
-    (doseq [{:keys [url out] :as _downloadable} downloadables]
-      (println "  " url)
-      (download-file url out)
-      (println "  done")))
-  (println "all done"))
+                           (map identity))
+        download-fn (fn [url out]
+                      (download-file url out)
+                      (info "  done"))]
+    (doseq [{:keys [url out]} downloadables]
+      (info "  getting" (.toString out) "from" url)
+      (if continueOnError
+        (try
+          (download-fn url out)
+          (catch Exception e
+            (error "  failed to get" (.toString out) "\n  " (.getMessage e))
+            (trace "  " (.toString e))))
+        (download-fn url out))))
+  (info "all done"))
 
 ;; $> bb -m dl.core
 ;; or
 ;; $> ./src/dl/core.clj
 (defn -main [& _args]
+  (timbre/set-level! :info)
   (run))
 
 (when (= *file* (System/getProperty "babashka.file"))
